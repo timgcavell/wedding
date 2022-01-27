ad8da32bb0abc3e8e32edde56c1df9f90f6806c0
Tim Cavell <tim@timgcavell.com>
2022-01-26 17:06:22 -0700

    Use lower quality images on homepage

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,11 @@
 import Image from "next/image";
 import Link from "next/link";
 import Header from "../components/header";
-import image1 from "../public/images/1.png";
-import image2 from "../public/images/2.png";
-import image3 from "../public/images/14.png";
-import image5 from "../public/images/3.png";
-import image11 from "../public/images/21.png";
+import image1 from "../public/images/1.jpeg";
+import image2 from "../public/images/2.jpeg";
+import image3 from "../public/images/14.jpeg";
+import image5 from "../public/images/3.jpeg";
+import image11 from "../public/images/21.jpeg";
 
 export const config = {
   unstable_runtimeJS: false,
@@ -25,11 +25,11 @@ export default function Index() {
           <div className="activities-link"><Link href="/austin">Austin</Link></div>
           <div className="rsvp-link"><Link href="/rsvp">RSVP</Link></div>
           <div className="registry-link"><Link href="/registry">Registry</Link></div>
-          <div className="image1"><Image priority quality="1" src={image1} /></div>
-          <div className="image2"><Image priority quality="1" src={image3} /></div>
-          <div className="image3"><Image priority quality="1" src={image5} /></div>
-          <div className="image4"><Image priority quality="1" src={image11} /></div>
-          <div className="image5"><Image priority quality="1" src={image2} /></div>
+          <div className="image1"><Image priority quality="30" src={image1} /></div>
+          <div className="image2"><Image priority quality="30" src={image3} /></div>
+          <div className="image3"><Image priority quality="30" src={image5} /></div>
+          <div className="image4"><Image priority quality="30" src={image11} /></div>
+          <div className="image5"><Image priority quality="30" src={image2} /></div>
         </nav>
       </main>
     </>
