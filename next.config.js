0f48095a4979d3625108aae4db45c8eb61453e82
Tim Cavell <tim@timgcavell.com>
2022-01-28 12:14:09 -0700

    Add postable redirect

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 module.exports = {
-    reactStrictMode: true,
-    poweredByHeader: false,
-}
+  reactStrictMode: true,
+  poweredByHeader: false,
+  async redirects() {
+    return [
+      {
+        source: "/postable",
+        destination: "https://www.postable.com/marytim",
+        permanent: false,
+        basePath: false,
+      },
+    ];
+  },
+};
