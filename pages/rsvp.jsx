20c623f17e17e50880bf574113528d6fde21b7db
Tim Cavell <tim@timgcavell.com>
2022-05-10 19:42:26 -0600

    Use jpeg images

--- pages/rsvp.jsx
+++ pages/rsvp.jsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Header from "../components/header";
-import hero from "../public/images/13.png";
+import hero from "../public/images/13.jpeg";
 
 export const config = {
   unstable_runtimeJS: false,
@@ -13,7 +13,7 @@ export default function RSVP() {
       <main className="wrapper">
         <h3>RSVP</h3>
         <h4>📫 Coming soon</h4>
-        <div className="full-bleed"><Image priority quality="1" src={hero} /></div>
+        <div className="full-bleed"><Image layout="responsive" priority src={hero} /></div>
       </main>
     </>
   )
