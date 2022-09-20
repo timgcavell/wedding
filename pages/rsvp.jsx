9985b53f8a3924cc3ecec92eccdfd1d61689744a
Tim Cavell <tim@timgcavell.com>
2022-09-20 11:48:32 -0500

    Add RSVP embed

--- pages/rsvp.jsx
+++ pages/rsvp.jsx
@@ -1,10 +1,4 @@
-import Image from "next/image";
 import Header from "../components/header";
-import hero from "../public/images/13.jpeg";
-
-export const config = {
-  unstable_runtimeJS: false,
-};
 
 export default function RSVP() {
   return (
@@ -12,8 +6,7 @@ export default function RSVP() {
       <Header />
       <main className="wrapper">
         <h3>RSVP</h3>
-        <h4>📫 Coming soon</h4>
-        <div className="full-bleed"><Image layout="responsive" priority src={hero} /></div>
+        <iframe src="https://maryandtimswedding.rsvpify.com?embed=1"></iframe>
       </main>
     </>
   )
