8fca6c6eba2fe682958800d67e09528bd21d9f66
Tim Cavell <tim@timgcavell.com>
2022-10-28 18:50:19 -0500

    Use newest image tag

--- pages/index.jsx
+++ pages/index.jsx
@@ -12,26 +12,44 @@ export const config = {
 };
 
 export default function Index() {
-  return (
-    <>
-      <Header />
-      <main>
-        <nav>
-          <div className="wedding-link container">
-            <Link href="/wedding">Wedding</Link>
-            {/* <div className="hover"><Image src={glimmer} /></div> */}
-          </div>
-          <div className="travel-link"><Link href="/lodging">Lodging</Link></div>
-          <div className="activities-link"><Link href="/austin">Austin</Link></div>
-          <div className="rsvp-link"><Link href="/rsvp">RSVP</Link></div>
-          <div className="registry-link"><Link href="/registry">Registry</Link></div>
-          <div className="image1"><Image priority quality="30" src={image1} /></div>
-          <div className="image2"><Image priority quality="30" src={image3} /></div>
-          <div className="image3"><Image priority quality="30" src={image5} /></div>
-          <div className="image4"><Image priority quality="30" src={image11} /></div>
-          <div className="image5"><Image priority quality="30" src={image2} /></div>
-        </nav>
-      </main>
-    </>
-  )
+  return <>
+    <Header />
+    <main>
+      <nav>
+        <div className="wedding-link container">
+          <Link href="/wedding">Wedding</Link>
+          {/* <div className="hover"><Image src={glimmer} /></div> */}
+        </div>
+        <div className="travel-link"><Link href="/lodging">Lodging</Link></div>
+        <div className="activities-link"><Link href="/austin">Austin</Link></div>
+        <div className="rsvp-link"><Link href="/rsvp">RSVP</Link></div>
+        <div className="registry-link"><Link href="/registry">Registry</Link></div>
+        <Image
+          className="image1"
+          priority
+          quality="30"
+          src={image1} />
+        <Image
+          className="image2"
+          priority
+          quality="30"
+          src={image3} />
+        <Image
+          className="image3"
+          priority
+          quality="30"
+          src={image5} />
+        <Image
+          className="image4"
+          priority
+          quality="30"
+          src={image11} />
+        <Image
+          className="image5"
+          priority
+          quality="30"
+          src={image2} />
+      </nav>
+    </main>
+  </>;
 }
--- pages/wedding.jsx
+++ pages/wedding.jsx
@@ -7,16 +7,23 @@ export const config = {
 };
 
 export default function Wedding() {
-  return (
-    <>
-      <Header extraClassName={"mobile-hidden"} />
-      <main className="wrapper">
-        <h3>Ceremony</h3>
-        <h4>Hotel Ella, Austin, TX</h4>
-        <h4>November 19, 2022, 5pm</h4>
-        <h4>Formal</h4>
-        <div className="full-bleed"><Image layout="responsive" priority src={hero} /></div>
-      </main>
-    </>
-  )
+  return <>
+    <Header extraClassName={"mobile-hidden"} />
+    <main className="wrapper">
+      <h3>Ceremony</h3>
+      <h4>Hotel Ella, Austin, TX</h4>
+      <h4>November 19, 2022, 5pm</h4>
+      <h4>Formal</h4>
+      <div className="full-bleed">
+        <Image
+          priority
+          src={hero}
+          sizes="100vw"
+          style={{
+            width: "100%",
+            height: "auto"
+          }} />
+      </div>
+    </main>
+  </>;
 }
