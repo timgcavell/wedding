5851c215092a370d9f3b93be98b98ae78cd32133
Tim Cavell <tim@timgcavell.com>
2022-11-09 16:32:49 -0600

    Close RSVPs

--- pages/rsvp.jsx
+++ pages/rsvp.jsx
@@ -1,4 +1,10 @@
+import Image from "next/image";
 import Header from "../components/header";
+import hero from "../public/images/13.jpeg";
+
+export const config = {
+  unstable_runtimeJS: false,
+};
 
 export default function RSVP() {
   return (
@@ -6,7 +12,18 @@ export default function RSVP() {
       <Header />
       <main className="wrapper">
         <h3>RSVP</h3>
-        <iframe src="https://maryandtimswedding.rsvpify.com?embed=1"></iframe>
+        <h4>Sorry, RSVPs are closed. We can’t wait to celebrate with you!</h4>
+        <div className="full-bleed">
+        <Image
+          priority
+          alt="Mary and Tim looking at each other and laughing"
+          src={hero}
+          sizes="100vw"
+          style={{
+            width: "100%",
+            height: "auto"
+          }} />
+      </div>
       </main>
     </>
   )
